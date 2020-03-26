c280c578ebd10967ad901ca1d97c18fbfb614178
josh <jhell85@gmail.com>
2020-03-26 14:12:59 -0700

    Add branching of triangle calculation

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
-	console.log('JavaScipt is working');
-
-	//on click function
 	$('form').submit(function(event) {
 		event.preventDefault();
-    console.log('click');
     let side1 = parseInt($("#side-1").val());
     let side2 = parseInt($("#side-2").val());
     let side3 = parseInt($("#side-3").val());
-    if(side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2){
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0){
+      alert("sides must be a positive number")
+      return
+    } else if(side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2){
       $(".output").replaceWith(`<h3 class="output">Not a Triangle</h3>`)
     } else if (side1 === side2 && side2 === side3){
       $(".output").replaceWith(`<h3 class="output">Equilateral Triangle</h3>`)
+		} else if (side1 === side2 || side2 === side3 || side1 === side3) {
+			$(".output").replaceWith(`<h3 class="output">Isosceles Triangle</h3>`)
+		} else if (side1 != side2 || side2 != side3 || side1 != side3) {
+			$(".output").replaceWith(`<h3 class="output">Scalene Triangle</h3>`)
 		}
 	});
 });
